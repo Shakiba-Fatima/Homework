a3054b035e3637fe9cb0c7342a8a01d3eedbb780
Shakiba-Fatima <fatimashakiba123@gmail.com>
2024-10-26 21:51:33 +0530

    addressofstructure.c

--- addressofstructure.c
+++ addressofstructure.c
@@ -14,16 +14,28 @@ int main(){
     printf("Address of S1: %p\n", p1);
     printf("Address of S2: %p\n", p2);
     printf("Address of S3: %p\n", p3);
-    //& is used to print the address to which the pointer is pointing.
-    //%p is used to print what address is stored in the pointer 
+   /*'p1' gives us the address where the variable s1 is stored or what is the value stored in p1, 
+    But if we write '&p1' instead of 'p1' then it will give us the address where the pointer is stored. 
+    %p format specifier 
+    we can see the outputs below are different for 'p1' and '&p1' which explains the above statements */
 return 0;
 }
 
 /*
+OUTPUT:
+ 
+o/p if we write 'p1' in print statement
 C:\Users\Administrator\Desktop\GITDEMO\ppwc-practice>gcc addressofstructure.c
+C:\Users\Administrator\Desktop\GITDEMO\ppwc-practice>a
+Address of S1: 0061FEF8
+Address of S2: 0061FEDC
+Address of S3: 0061FEC0
 
+o/p if we write '&p1' in print statement
+C:\Users\Administrator\Desktop\GITDEMO\ppwc-practice>gcc addressofstructure.c
 C:\Users\Administrator\Desktop\GITDEMO\ppwc-practice>a
 Address of S1: 0061FF00
 Address of S2: 0061FEE0
 Address of S3: 0061FEC0
+
 */
